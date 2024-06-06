8633ca13383ac489c61a5000fd950344e43b4ef3
Sardar Nazeer <sardarnazeer1108@gmail.com>
2024-06-06 04:56:55 -0700

    myApp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import chatGptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import msgIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='sidebar'>
+        <div className='upperSide'>
+          <div className='upperSideTop'><img src={chatGptLogo} alt="logo picture" className="logo" /><span className="brand">ChatGPT</span></div>
+          <button className="midBtn"><img src={addBtn} alt="new chat" className="addBtn" />New Chat</button>
+          <div className="upperSideBottom">
+            <button className="query"><img src={msgIcon} alt="msg icon" />What is programming ?</button>
+            <button className="query"><img src={msgIcon} alt="msg icon" />How to use an API ?</button>
+          </div>
+        </div>
+        <div className='lowerSide'>
+          <div className="listItem"><img src={home} alt="list item image" className="listItemImg" />Home</div>
+          <div className="listItem"><img src={saved} alt="list item image" className="listItemImg" />Saved</div>
+          <div className="listItem"><img src={rocket} alt="list item image" className="listItemImg" />Upgrade to pro</div>
+        </div>
+      </div>
+      <div className='main'>
+
+      </div>
     </div>
   );
 }
